82d808a1a31cd6932c98298753ce5467df00ade4
CFM-jpg <2010vic.martin@gmail.com>
2026-05-05 22:33:31 +0200

    maj

--- src/app/api/page.tsx
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-export default function Home() {
-  const html = readFileSync(join(process.cwd(), 'public', 'index.html'), 'utf-8');
-  return (
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-  );
-}
